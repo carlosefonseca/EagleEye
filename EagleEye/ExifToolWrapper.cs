76933da65e92115ea6b9534adfa5151069afdc17
Carlos <carlosefonseca@gmail.com>
2011-06-10 14:13:10 +0100

    Fix para o caso de não haver retorno do exiftool
    Signed-off-by: Carlos <carlosefonseca@gmail.com>

--- EagleEye/ExifToolWrapper.cs
+++ EagleEye/ExifToolWrapper.cs
@@ -7,43 +7,42 @@ using System.IO;
 using Newtonsoft.Json;
 using EagleEye.Common;
 
-namespace EagleEye
-{
-    class ExifToolWrapper
-    {
-        private static Process p;
-        private static ProcessStartInfo start;
-        private static StreamReader reader;
+namespace EagleEye {
+	class ExifToolWrapper {
+		private static Process p;
+		private static ProcessStartInfo start;
+		private static StreamReader reader;
 
-        public static List<Image> CrawlDir(string dir)
-        {
-            start = new ProcessStartInfo();
+		public static List<Image> CrawlDir(string dir) {
+			start = new ProcessStartInfo();
 			start.StandardOutputEncoding = Encoding.GetEncoding(1252);
-            start.FileName = @"exiftool.exe";
-            start.Arguments = "-j -q -r -ext jpg -L \"" + dir + "\"";
-            start.UseShellExecute = false;
-            start.RedirectStandardOutput = true;
+			start.FileName = @"exiftool.exe";
+			start.Arguments = "-j -q -r -ext jpg -L \"" + dir + "\"";
+			start.UseShellExecute = false;
+			start.RedirectStandardOutput = true;
 
-            p = Process.Start(start);
-            reader = p.StandardOutput;
+			p = Process.Start(start);
+			reader = p.StandardOutput;
 
-            Console.WriteLine("Running exiftool...");
-            string txt = reader.ReadToEnd();
-            Console.WriteLine("Converting data...");
-            List<Dictionary<string, object>> exifdata = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(txt);
+			Console.WriteLine("Running exiftool...");
+			string txt = reader.ReadToEnd();
+			Console.WriteLine("Converting data...");
+			List<Dictionary<string, object>> exifdata = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(txt);
 
-            Console.WriteLine("Adding to the DB.");
-            List<Image> images = new List<Image>();
-            Image im;
-            foreach (Dictionary<string, object> it in exifdata)
-            {
-                im = new Image(it["SourceFile"].ToString());
-                im.Exif(it);
-                images.Add(im);
-            }
+			Console.WriteLine("Adding to the DB.");
+			List<Image> images = new List<Image>();
+			Image im;
+			if (exifdata == null) {
+			} else {
+				foreach (Dictionary<string, object> it in exifdata) {
+					im = new Image(it["SourceFile"].ToString());
+					im.Exif(it);
+					images.Add(im);
+				}
+			}
 
-            return images;
-        }
+			return images;
+		}
 
 		public static void CrawlDir(string dir, ImageCollection collection) {
 			start = new ProcessStartInfo();
@@ -70,5 +69,5 @@ namespace EagleEye
 			}
 		}
 
-    }
+	}
 }
