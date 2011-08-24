417e7de6f11a179016bc65dfbd3aa2f6aae00481
Carlos Fonseca <carlosefonseca@gmail.com>
2011-08-24 09:34:13 +0100

    Adjustments

--- DeepZoomView/Properties/AssemblyInfo.cs
+++ DeepZoomView/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeepZoomView")]
+[assembly: AssemblyTitle("EagleEye")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Avanade")]
-[assembly: AssemblyProduct("DeepZoomView")]
-[assembly: AssemblyCopyright("Copyright © Avanade 2008")]
+[assembly: AssemblyCompany("IST")]
+[assembly: AssemblyProduct("EagleEye")]
+[assembly: AssemblyCopyright("Copyright © Carlos Fonseca 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
